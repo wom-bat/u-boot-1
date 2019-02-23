45dc8b5e7fc686deeca4d2718facadd298106c63
simon <simon@yeacreate.com>
2019-02-23 11:34:26 +0800

    updated 20190223

--- include/configs/870a_rk3288.h
+++ include/configs/870a_rk3288.h
@@ -2,6 +2,7 @@
  * (C) Copyright 2016 Rockchip Electronics Co., Ltd
  *
  * SPDX-License-Identifier:     GPL-2.0+
+//	"load mmc 0:5 ${kernel_addr_r} /boot/zImage; load mmc 0:5 ${fdt_addr_r} /boot/rk3288-ntablet-870a-5648.dtb; bootz ${kernel_addr_r} - ${fdt_addr_r};"
  */
 
 #ifndef __CONFIG_H
@@ -13,6 +14,18 @@
 		"stderr=serial,vidconsole\0"
 
 #include <configs/rk3288_common.h>
+
+#ifndef CONFIG_SPL_BUILD
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND LINUX_BOOTCOMMAND
+#endif
+
+
+#define LINUX_BOOTCOMMAND \
+	"env set bootargs console=ttyS2,115200 rw rootfstype=ext4 root=/dev/mmcblk1p6 earlyprintk;" \
+	"boot_android ${devtype} ${devnum};" \
+	"bootrkp;"
+	
 
 #define CONFIG_SYS_MMC_ENV_DEV 0
 
