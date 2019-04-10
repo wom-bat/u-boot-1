ffa67dde3a5a855bf2c297e8cf7c13407ec926ed
simon <simon@yeacreate.com>
2019-04-10 14:05:37 +0800

    ready to meg

--- board/ntablet/870a_rk3288/870a_rk3288.c
+++ board/ntablet/870a_rk3288/870a_rk3288.c
@@ -4,6 +4,10 @@
  * SPDX-License-Identifier:     GPL-2.0+
  */
 
+#include <asm/io.h>
 #include <common.h>
 #include <dm.h>
 #include <spl.h>
+#include <i2c.h>
+#include <power/rk8xx_pmic.h>
+
--- include/configs/870a_rk3288.h
+++ include/configs/870a_rk3288.h
@@ -16,26 +16,30 @@
 #include <configs/rk3288_common.h>
 
 #undef BOOT_TARGET_DEVICES
-
-#define BOOT_TARGET_DEVICES(func) \
-	func(MMC, mmc, 1) \
-	func(USB, usb, 0) \
-	func(PXE, pxe, na) \
-	func(DHCP, dchp, na)
-
+#undef BOOTENV
+#define BOOT_TARGET_DEVICES(func)
+#define BOOTENV
 
 #ifndef CONFIG_SPL_BUILD
 #undef CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND LINUX_BOOTCOMMAND
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS LINUX_BOOTMENU
 #endif
 
+#undef ENV_MEM_LAYOUT_SETTINGS
+#define ENV_MEM_LAYOUT_SETTINGS \
+	"scriptaddr=0x00000000\0" \
+	"fdt_addr_r=0x08300000\0" \
+	"kernel_addr_r=0x02008000\0"
 
-#define LINUX_BOOTCOMMAND \
-	"env set bootargs console=ttyS2,115200 rw rootfstype=ext4 root=/dev/mmcblk1p6 earlyprintk;" \
-	"boot_android ${devtype} ${devnum};" \
-	"bootrkp;"
-	
+#define LINUX_BOOTMENU \
+	ENV_MEM_LAYOUT_SETTINGS \
+	"importbootenv=env import -t -r ${scriptaddr} $filesize\0" \
+	"loadbootenv=load ${devtype} ${devnum}:5 ${scriptaddr} /boot/uEnv.txt\0" \
+	"preboot=run loadbootenv;run importbootenv\0" \
+	BOOTENV	
 
-#define CONFIG_SYS_MMC_ENV_DEV 1
+#define CONFIG_SYS_MMC_ENV_DEV 0
+#define CONFIG_MENU_SHOW
 
 #endif
