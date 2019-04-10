c78dfebde88b9f1c23e83df1cb9a5ab8c97a207b
simon <simon@yeacreate.com>
2019-04-10 16:15:43 +0800

    fix the wifi clk and add rockuse mode if boot false

--- include/configs/870a_rk3288.h
+++ include/configs/870a_rk3288.h
@@ -34,10 +34,12 @@
 
 #define LINUX_BOOTMENU \
 	ENV_MEM_LAYOUT_SETTINGS \
+	"usbboot=rockusb 0 mmc 0\0" \
+	"load_wifi_clk=pmic dev pmic@1c;pmic write 0x20 0x01\0" \
 	"importbootenv=env import -t -r ${scriptaddr} $filesize\0" \
 	"loadbootenv=load ${devtype} ${devnum}:5 ${scriptaddr} /boot/uEnv.txt\0" \
-	"preboot=run loadbootenv;run importbootenv\0" \
-	BOOTENV	
+	"preboot=run load_wifi_clk;run loadbootenv;run importbootenv\0" \
+	BOOTENV
 
 #define CONFIG_SYS_MMC_ENV_DEV 0
 #define CONFIG_MENU_SHOW
