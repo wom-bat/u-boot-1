72b469dda1b307fc82cdddc2007d19ed2f382c83
simon <simon@yeacreate.com>
2019-04-18 12:38:21 +0800

    fix up boot logo

--- include/configs/870a_rk3288.h
+++ include/configs/870a_rk3288.h
@@ -16,3 +16,34 @@
 		"stderr=serial,vidconsole\0"
 
 #endif
+
+#undef BOOT_TARGET_DEVICES
+#undef BOOTENV
+#define BOOT_TARGET_DEVICES(func)
+#define BOOTENV
+
+#ifndef CONFIG_SPL_BUILD
+#undef CONFIG_BOOTCOMMAND
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	ENV_MEM_LAYOUT_SETTINGS \
+	ROCKCHIP_DEVICE_SETTINGS \
+	LINUX_BOOTMENU \
+	BOOTENV
+#endif
+
+
+#undef ENV_MEM_LAYOUT_SETTINGS
+#define ENV_MEM_LAYOUT_SETTINGS \
+	"scriptaddr=0x00000000\0" \
+	"fdt_addr_r=0x08300000\0" \
+	"kernel_addr_r=0x02008000\0"
+
+#define LINUX_BOOTMENU \
+	"usbboot=rockusb 0 mmc 0\0" \
+	"load_wifi_clk=pmic dev pmic@1c;pmic write 0x20 0x01\0" \
+	"importbootenv=env import -t -r ${scriptaddr} $filesize\0" \
+	"loadbootenv=load ${devtype} ${devnum}:5 ${scriptaddr} /boot/uEnv.txt\0" \
+	"preboot=run load_wifi_clk;run loadbootenv;run importbootenv\0"
+
+#define CONFIG_MENU_SHOW
